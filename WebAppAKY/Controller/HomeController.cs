55ff0f59761fce7f7bbc789e9367432614fa743a
akayee <53110296+akayee@users.noreply.github.com>
2021-05-18 15:20:40 +0300

    contoller yapıldı

--- WebAppAKY/Controller/HomeController.cs
+++ WebAppAKY/Controller/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿
+using AKYSTRATEJI.ViewModals;
 using BL.Abstract;
 using Microsoft.AspNetCore.Authorization;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace WebAppAKY.Controller
 {
@@ -12,11 +14,25 @@ namespace WebAppAKY.Controller
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IStratejiServis _stratejiServis;
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IStratejiServis stratejiServis)
         {
             _logger = logger;
+            _stratejiServis = stratejiServis;
         }
 
+        public IActionResult StratejiBilgileriGetir()
+        {
+            var model = new StratejiBilgileri()
+            {
 
+            };
+            return View(model);
+
+        }
+
+        private IActionResult View(StratejiBilgileri model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
