d3ecd08a38fc3f521968f668e5596979412ad978
akayee <53110296+akayee@users.noreply.github.com>
2021-05-18 15:03:45 +0300

    İşyeri Controller Yazıldı

---
+++ WebAppAKY/Controller/HomeController.cs
@@ -0,0 +1,22 @@
+﻿
+using BL.Abstract;
+using Microsoft.AspNetCore.Authorization;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebAppAKY.Controller
+{
+    [Authorize]
+    class HomeController: ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IStratejiServis _stratejiServis;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+    }
+}
